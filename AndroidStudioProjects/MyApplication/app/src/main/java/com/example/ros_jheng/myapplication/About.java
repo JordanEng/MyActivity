16190ef6a1553748bef04b8d34d09058ef01a2b8
Jordan <jordyeng@gmail.com>
2016-04-04 08:46:00 -0700

    k

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/ros_jheng/myapplication/About.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/ros_jheng/myapplication/About.java
@@ -1,29 +1,21 @@
 package com.example.ros_jheng.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-public class About extends AppCompatActivity {
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+public class About extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "You have no mail", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        {
+            TextView v = (TextView) findViewById(R.id.text_target);
+            v.setText(Html.fromHtml(getString(R.string.about_text)));
+            v.setMovementMethod(LinkMovementMethod.getInstance());
+            v.setLinksClickable(true);
+        }
     }
-
 }
